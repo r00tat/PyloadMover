d6ed7e9bd9ef1e443c8b8b132de42efc2fca0932
Paul Woelfel <paul.woelfel@nts.eu>
2014-01-04 14:47:35 +0100

    use tabs instead of spaces

--- PyloadMover.py
+++ PyloadMover.py
@@ -1,19 +1,19 @@
 from module.plugins.Hook import Hook
-
+	
 class PyloadMover(Hook):
 	"""
-    Moves files to movie or series folder.
-    """
-    __name__ = "PyloadMover"
-    __version__ = "0.1"
-    __description__ = "Moves finished downloads to movies or series folders."
-    __config__ = [ ("activated" , "bool" , "Activated"  , "True" ),
-    	("movieSize" , "int" , "Treat files larger than this MB as movie"  , "2000" ),
-    	("moviesPath" , "str" , "Folder for movies"  , "/share/Multimedia/Filme" ),
-    	("seriesPath" , "str" , "Folder for series"  , "/share/Multimedia/Serien" ), ]
-    #__threaded__ = ["downloadFinished"]
-    __author_name__ = ("Paul Woelfel")
-    __author_mail__ = ("pyload@frig.at")
+	Moves files to movie or series folder.
+	"""
+	__name__ = "PyloadMover"
+	__version__ = "0.1"
+	__description__ = "Moves finished downloads to movies or series folders."
+	__config__ = [ ("activated" , "bool" , "Activated"  , "True" ),
+		("movieSize" , "int" , "Treat files larger than this MB as movie"  , "2000" ),
+		("moviesPath" , "str" , "Folder for movies"  , "/share/Multimedia/Filme" ),
+		("seriesPath" , "str" , "Folder for series"  , "/share/Multimedia/Serien" ), ]
+	#__threaded__ = ["downloadFinished"]
+	__author_name__ = ("Paul Woelfel")
+	__author_mail__ = ("pyload@frig.at")
 
 
 	event_map = {"downloadFinished" : "downloadFinished",
